d35f489cf3650055ede77592315f4ab14ac8673f
sasidhar0809-web <sasidhar0809@gmail.com>
2026-06-10 22:53:44 -0700

    Fixed failure

--- Pages/AdminPage.ts
+++ Pages/AdminPage.ts
@@ -1,107 +1,127 @@
+import { Page, expect } from '@playwright/test';
+import { ElementActions } from '../utils/Elements';
+import { assert } from 'node:console';
 
-import { test } from '@playwright/test';
-import { AdminPage } from '../Pages/AdminPage';
-import { faker } from '@faker-js/faker';
+export class AdminPage {
 
-test('Create New Admin User', async ({ page }) => {
-  const admin = new AdminPage(page);
-    await page.goto('/web/index.php/dashboard/index');
-    const username = faker.internet.username();
-  const data = {
-    employeeName: 'Ad',
-    username: username,
-    password: 'Password123'
-  };
+  private actions: ElementActions;
 
-  console.log('User:', data);
+  constructor(private page: Page) {
+    this.actions = new ElementActions(page);
+  }
 
-  await admin.goToAdmin();
-  await admin.clickAdd();
-  await admin.fillAdminForm(data);
-  await admin.saveUser();
-  await admin.verifySuccess();
-  await admin.goToAdmin();
-  await admin.searchUser(username);
-});
+  adminMenu = this.page.locator('text=Admin').nth(0);
+  addBtn = this.page.getByRole('button', { name: 'Add' });
 
+  userRoleDropdown = this.page.locator('.oxd-select-text-input').nth(0);
+  adminOption = this.page.locator('text=Admin').nth(2);
 
+  employeeName = this.page.locator('input[placeholder="Type for hints..."]');
+  usernameField = this.page.locator('(//input[@class="oxd-input oxd-input--active"])[2]');
 
-test('Validate Duplicate Username', async ({ page }) => {
+  statusDropdown = this.page.locator('.oxd-select-text-input').nth(1);
+  enabledOption = this.page.locator('text=Enabled');
 
-  const admin = new AdminPage(page);
+  passwordField = this.page.locator('(//input[@type="password"])[1]');
+  confirmPasswordField = this.page.locator('(//input[@type="password"])[2]');
 
-  await page.goto('/web/index.php/dashboard/index');
+  saveBtn = this.page.getByRole('button', { name: 'Save' });
 
-  const username = faker.internet.username();
-
-  const data = {
-    employeeName: 'Ad',
-    username: username,
-    password: 'Password123'
-  };
-
-  await admin.goToAdmin();
-  await admin.clickAdd();
-  await admin.fillAdminForm(data);
-  await admin.saveUser();
-  await admin.verifySuccess();
-
-  await admin.clickAdd();
-  await admin.fillAdminForm(data);
-  await admin.saveUser();
-
-  await admin.verifyDuplicate();
-});
+  successMsg = this.page.locator('text=Successfully');
+  duplicateMsg = this.page.locator('.oxd-input-field-error-message, [role="alert"]');
 
 
-test('Search Admin User', async ({ page }) => {
-
-  const admin = new AdminPage(page);
-  await page.goto('/web/index.php/dashboard/index');
-
-  await admin.goToAdmin();
-  await admin.searchUser('Admin');
-
-  await admin.validateSearchResult('Admin');
-});
+  searchUsername = this.page.locator('(//input[@class="oxd-input oxd-input--active"])[2]');
+  searchBtn = this.page.getByRole('button', { name: 'Search' });
+  resetBtn = this.page.getByRole('button', { name: 'Reset' });
 
 
-test('Reset Search', async ({ page }) => {
-
-  const admin = new AdminPage(page);
-  await page.goto('/web/index.php/dashboard/index');
-
-  await admin.goToAdmin();
-  await admin.searchUser('Admin');
-
-  await admin.resetSearch();
-});
+  deleteIcon = this.page.locator('.oxd-icon.bi-trash').nth(0);
+  confirmDeleteBtn = this.page.getByRole('button', { name: 'Yes, Delete' });
 
 
+  async selectEmployeeName(name: string) {
+    await this.employeeName.pressSequentially(name);
+    await this.employeeName.press('Backspace');
+    await this.page.waitForTimeout(2000); // Wait for suggestions to load
 
-test('Delete Admin User', async ({ page }) => {
+    await this.page.waitForSelector('.oxd-autocomplete-dropdown');
 
-  const admin = new AdminPage(page);
-  await page.goto('/web/index.php/dashboard/index');
+    await this.employeeName.press('ArrowDown');
+    await this.employeeName.press('Enter');
+  }
 
-  const username = faker.internet.username();
+  async goToAdmin() {
+    await this.actions.click(this.adminMenu);
+  }
 
-  const data = {
-    employeeName: 'Ad',
-    username,
-    password: 'Password123'
-  };
+  async clickAdd() {
+    await this.actions.click(this.addBtn);
+  }
 
-  await admin.goToAdmin();
-  await admin.clickAdd();
-  await admin.fillAdminForm(data);
-  await admin.saveUser();
-  await admin.verifySuccess();
-  await admin.goToAdmin();
-  await admin.searchUser(username);
-  await admin.deleteUser();
-  await admin.resetSearch();
-  await admin.negativeSearchUser(username);
-});
+  async fillAdminForm(data: any) {
 
+    await this.actions.click(this.userRoleDropdown);
+    await this.actions.click(this.adminOption);
 
+    await this.selectEmployeeName(data.employeeName);
+
+    await this.actions.fill(this.usernameField, data.username);
+
+    await this.actions.click(this.statusDropdown);
+    await this.actions.click(this.enabledOption);
+
+    await this.actions.fill(this.passwordField, data.password);
+    await this.actions.fill(this.confirmPasswordField, data.password);
+  }
+
+  async saveUser() {
+    await this.actions.click(this.saveBtn);
+  }
+
+  async verifySuccess() {
+    await expect(this.successMsg).toBeVisible();
+    await this.actions.takeScreenshot('UserCreated');
+  }
+
+  async verifyDuplicate() {
+    await expect(this.duplicateMsg).toBeVisible();
+    await this.actions.takeScreenshot('DuplicateError');
+  }
+
+  async searchUser(username: string) {
+    await this.searchUsername.waitFor({ state: 'visible' });
+    assert(await this.searchUsername.isVisible(), 'Search username field is not visible');
+    await this.actions.fill(this.searchUsername, username);
+    await this.actions.click(this.searchBtn);
+  }
+
+  async negativeSearchUser(username: string) {
+   
+    assert(!(await this.searchUsername.isVisible(), 'Search username field is visible'));
+  
+  }
+
+  async validateSearchResult(username: string) {
+    await expect(this.page.locator('.oxd-table-body')).toContainText(username);
+    assert((await this.page.locator('.oxd-table-body').innerText()).includes(username), 'Search result does not contain the expected username');
+    await this.actions.takeScreenshot('SearchResult');
+    await this.deleteIcon.waitFor({ state: 'visible' });
+  }
+
+  async resetSearch() {
+    await this.actions.click(this.resetBtn);
+    await expect(this.searchUsername).toHaveValue('');
+  }
+
+  async deleteUser() {
+    await this.deleteIcon.waitFor({ state: 'visible' });
+    await this.deleteIcon.scrollIntoViewIfNeeded();
+    await this.actions.click(this.deleteIcon);
+    await this.confirmDeleteBtn.waitFor({ state: 'visible' });
+    await this.actions.click(this.confirmDeleteBtn);
+
+    await this.page.waitForSelector('text=Successfully Deleted');
+    await this.actions.takeScreenshot('UserDeleted');
+  }
+}
