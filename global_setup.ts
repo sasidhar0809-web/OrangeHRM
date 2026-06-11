3794f7a92e381e8cd41e986d62354655a88854a5
sasidhar0809-web <sasidhar0809@gmail.com>
2026-06-10 21:43:16 -0700

    Add files via upload

---
+++ global_setup.ts
@@ -0,0 +1,37 @@
+
+import { chromium } from '@playwright/test';
+import { getBaseURL } from './utils/env';
+
+async function globalSetup() {
+
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  const url = getBaseURL();
+
+  console.log('Running ENV URL:', url);
+
+  // Navigate
+  await page.goto(url);
+  //await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 60000 });
+
+
+  if (url.includes('orangehrmlive')) {
+
+    await page.fill('input[placeholder="Username"]', 'Admin');
+    await page.fill('input[placeholder="Password"]', 'admin123');
+    await page.click('button[type="submit"]');
+
+    await page.waitForSelector('text=Dashboard');
+
+    await page.context().storageState({
+      path: 'storageState.json'
+    });
+
+
+  }
+
+  await browser.close();
+}
+
+export default globalSetup;
---
+++ playwright.config.ts
@@ -0,0 +1,102 @@
+ import * as  dotenv from 'dotenv';
+dotenv.config();
+import { defineConfig, devices } from '@playwright/test';
+import { getBaseURL } from './utils/env';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+
+// import path from 'path';
+ 
+//console.log('ENV VALUE FROM CONFIG:', process.env.ENV);
+console.log('BASE_URL from ENV:', process.env.BASE_URL);
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  
+testDir: './tests',
+
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  timeout: 60000,
+  reporter: [['html', { open: 'never' }]],
+
+  globalSetup: './global_setup.ts',
+
+  use: {
+    screenshot: 'on',
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    
+{
+      name: 'login-tests',
+      testMatch: /login\.spec\.ts/,
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: getBaseURL(),
+        storageState: undefined
+      }
+    },
+
+    {
+      name: 'admin-tests',
+      testMatch: /AdminPage\.spec\.ts/,
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: getBaseURL(),
+        storageState: 'storageState.json'
+      }
+    },
+  
+
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
